f5d658f097e425637950dc05ce94a8c76602f6e4
Wong Kai Wey <75353882+kelv26@users.noreply.github.com>
2021-01-07 20:17:32 +0800

    Update 1
    Added 'requirements.txt' and 'setup.py' to setup easily.

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Python File used to try all the voices of gTTS and pyttsx3.
 
 # Dependencies
 -gTTS <br>
--pyttsx3
+-pyttsx3 <br>
+-playsound
+
+# Setup and Run
+To setup the dependencies before using the Python Application please use the command `python setup.py`. After setting up, use the command `python app.py` to launch the application.
--- gttspython.py
+++ gttspython.py
@@ -2,14 +2,21 @@ from gtts import gTTS
 from playsound import playsound
 import time
 import os
+import os.path
 
 """This function will take a string as input
 convert it into voice and save it as a file temp.mp3
 play it and delete it"""
 def speak(temps):
     tts = gTTS(temps, lang="en")
-    print('gTTS: ',temps)
-    tts.save("./temp/temp1.mp3")
-    playsound('./temp/temp1.mp3')
-    time.sleep(0.2)
-    os.remove("./temp/temp1.mp3")
+
+    if not os.path.exists("/temp"):
+        os.makedirs("/temp")
+        print("Please try again. (created 'temp' folder for Google .mp3 files.")
+    else:
+        print('gTTS: ',temps)
+        tts.save("/temp/temp1.mp3")
+        playsound('/temp/temp1.mp3')
+        time.sleep(0.2)
+        os.remove("/temp/temp1.mp3")
+    
